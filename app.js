a530786572ca94483cb2d413fd01ce4c2de3baa4
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-10-19 14:02:35 +0300

    dfshgdrtyhdr

--- app.js
+++ app.js
@@ -22,10 +22,12 @@ document.addEventListener('click', event => {
 
         node.classList.toggle('fa-lock-open');  //зміна класу на розблок.
         node.classList.toggle('fa-lock');   //зміна класу на блок.
+    } else if (type === 'copy') {
+        copyToClickboard(event.target.textContent);
     }
 })
 
-// генератор ХЕКС кольора
+// генератор ХЕКС кольора   // chroma.random()
 function generateRandomColor() {
     const hexCodes = '123456789ABCDEF';
     let color ='';
@@ -35,14 +37,27 @@ function generateRandomColor() {
     return '#' + color;
 }
 
-function setRandomColors() {
-cols.forEach((col) => {
+function copyToClickboard(text) {
+    return navigator.clipboard.writeText(text);
+}
+
+function setRandomColors(isInitial) {
+    const colors = isInitial ? getColorsFromHash() : [];
+
+cols.forEach((col, index) => {
     const isLocked = col.querySelector('i').classList.contains('fa-lock'); // перевірка чи заблокована колонка;
     const text = col.querySelector('h2');  // перевірка кольору напису
     const button = col.querySelector('button');  //перевірка кольору кнопки
-    const color =  generateRandomColor();
+    
+    if (isLocked) {
+          
+        colors.push(text.textContent)
+        return; // блокировка зміни кольору
+    }  
 
-    if (isLocked) {return}  // блокировка зміни кольору
+    const color = isInitial ? colors[index] ? colors[index] : generateRandomColor() : generateRandomColor(); // chroma.random()
+
+    colors.push(color)
 
     text.textContent = color;      // зміна тексту 
     col.style.background = color;  //зміна кольору
@@ -50,6 +65,8 @@ cols.forEach((col) => {
     setTextColor(text, color);  // зміна кольору тексту
     setTextColor(button, color);   //зніна кольору іконки кнопки
 })
+
+updateColorsHash(colors);
 }
 
 // функція зміни кольору текста
@@ -58,4 +75,15 @@ function setTextColor(text, color) {
  text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
-setRandomColors()
+function updateColorsHash(colors = []) {
+    document.location.hash = colors.map((col) => col.toString().substring(1)).join('-');
+}
+
+function getColorsFromHash() {
+    if (document.location.hash.length > 1) {
+       return document.location.hash.substring(1).split('-').map(color => '#' + color);
+    }
+    return [];
+}
+
+setRandomColors(true)
